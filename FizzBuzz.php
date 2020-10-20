a5831e59c2b0e12acc1a51a78e7d3162d54639dd
Thibaut <thibaut.t.gerard@student.vinci.be>
2020-10-20 17:13:18 +0200

    add - 2 tests

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,11 +1,17 @@
 <?php
-namespace FizzBuzz;
+
 
 class FizzBuzz
 {
-    public function play(int $number) {
 
+
+    public function count(int $int): string
+    {
+        if($int === 2) return "2";      //guard
+
+        return "1";
     }
+
 
 
 }
--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -2,10 +2,38 @@
 require "vendor/autoload.php";
 require "FizzBuzz.php";
 
-use FizzBuzz\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
+
+    private $fizzBuzz;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fizzBuzz = new FizzBuzz();
+    }
+
+
+    public function test_Count_Given1_shouldReturn1() {
+        //Arrange      on prepare nos variable et outils pour le test
+        /*$fizzBuzz = new FizzBuzz();*/
+
+        //Act
+        $actual = $fizzBuzz->count(1);
+
+        //Assert
+        $this->assertEquals("1", $actual);
+        //$this->assertEquals(true, true);
+    }
+
+    public function test_count_given2_schouldReturn2() {
+        $fizzBuzz = new FizzBuzz();
+
+        $actual = $fizzBuzz->count(2);
+
+        $this->assertEquals("2", $actual);
+    }
 
 }
